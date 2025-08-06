3a3fc3ffc4cf86eef7a4ba63ae99c75ca62a62e5
aimerickrol1 <aimeric.66666@gmail.com>
2025-08-06 23:21:26 +0100

    Repositionner les boutons de priorité

--- app/(tabs)/note/create.tsx
+++ app/(tabs)/note/create.tsx
@@ -339,6 +339,7 @@ export default function CreateNoteScreen() {
           </View>
         </View>
 
+
     </View>
   );
 }
--- app/(tabs)/note/edit/[id].tsx
+++ app/(tabs)/note/edit/[id].tsx
@@ -389,6 +389,7 @@ export default function EditNoteScreen() {
           </View>
         </View>
 
+
     </View>
   );
 }
